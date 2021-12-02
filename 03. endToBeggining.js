6b7dbb6865e97643a75621f04d9c4933110a25ee
AvetBadalyan <avetbadalyan@gmail.com>
2021-12-02 21:42:17 +0400

    feat: Bring the last digit  to the beginning

--- 03. endToBeggining.js
+++ 03. endToBeggining.js
@@ -1,1 +1,13 @@
 //Bring the last digit of the number to the beginning.
+//NEED TO FIX BUGS
+let arr = [367, 1002, 250, 8];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 10 == 0 || arr[i].length == 1) {
+    console.log(arr[i]);
+  } else {
+    let lastDigit = arr[i] % 10;
+    let newNumber = (arr[i] - lastDigit) / 10;
+    let result = "" + lastDigit + newNumber;
+    console.log(result);
+  }
+}
