694194076dfba3849cc7ca63259d1d35c60337e7
AvetBadalyan <avetbadalyan@gmail.com>
2021-12-05 23:37:23 +0400

    fix: fixed some bugs

--- 17. splitString.js
+++ 17. splitString.js
@@ -1,7 +1,7 @@
 // Given a sentence as a string. Split it according to space and comma and create an array consisting of
 // the words of the array. The last word should not contain the last . or ! .
 
-let sentence = "Keep your friends close, but your enemies closer.";
+let sentence = "May the Force be with you.";
 let array = sentence.split(" ");
 
 array[array.length - 1] = array[array.length - 1]
